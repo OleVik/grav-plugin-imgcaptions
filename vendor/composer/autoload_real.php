ca44976712c9856b732a619dd299bd93e143d29e
A---- <alan71@free.fr>
2020-03-02 22:21:42 +0100

    Proper composer.json + .gitignore

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInite678b47c7555cb0861d015705620df54
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInite678b47c7555cb0861d015705620df54::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequiree678b47c7555cb0861d015705620df54($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequiree678b47c7555cb0861d015705620df54($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
